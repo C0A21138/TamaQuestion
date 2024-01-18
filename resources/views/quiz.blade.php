f07ab8aa49800b47ac694fc67ac75f770f759166
YudaiAbe10 <148335064+YudaiAbe10@users.noreply.github.com>
2024-01-18 18:35:06 +0900

    [update]figmaに沿って作成

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -6,7 +6,51 @@
 
 @section('content')
     <div style="text-align: center;">
-        <p>このページは開発途中です</p>
+        <a href="https://www.teu.ac.jp/">
+        <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 100px;">
+        <font color="#000000">
+        クイズ
+        <font>
+        </div>
+        </a>
+        <br>
+        <div style="text-align:left;">
+        わからない場合
+        </div>
+        <br>
+        <a href="https://www.teu.ac.jp/">
+        <div style="padding: 20px; margin-bottom: 20px; border: 1px solid #333333; border-radius: 10px;">
+        <font color="#000000">
+        今日のおすすめクイズ
+        <font>
+        </div>
+        </a>
+        <br>
+        <p>ジャンル</p>
+        <a href="https://www.teu.ac.jp/">
+        <div style="padding: 20px; margin-bottom: 20px; border: 1px solid #333333; border-radius: 10px;">
+        <font color="#000000">
+        アニメクイズ
+        <font>
+        </div>
+        </a>
+        <br>
+        <a href="https://www.teu.ac.jp/">
+        <div style="padding: 20px; margin-bottom: 20px; border: 1px solid #333333; border-radius: 10px;">
+        <font color="#000000">
+        ドラマ・映画クイズ
+        <font>
+        </div>
+        </a>
+        <br>
+        <a href="https://www.teu.ac.jp/">
+        <div style="padding: 20px; margin-bottom: 20px; border: 1px solid #333333; border-radius: 10px;">
+        <font color="#000000">
+        鉄道クイズ
+        <font>
+        </div>
+        </a>
+        <br>
         <p>クイズ機能を試す場合は<br>以下のリンクをクリック！</p>
         <a href="https://www.figma.com/proto/6zGQZa98IYpUt1Gi2vMtTQ/%E3%82%A2%E3%83%97%E3%83%AA%E3%81%AE%E3%83%97%E3%83%AD%E3%83%88%E3%82%BF%E3%82%A4%E3%83%97?type=design&node-id=43-93&t=RCoFvkTa4GwX3Z5e-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=43%3A93&show-proto-sidebar=1&mode=design">
             プロトタイプへ移動する
